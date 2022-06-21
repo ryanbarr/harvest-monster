3c17fa5718bc19522a01835338fdf4967dff3999
Ryan Barr <ryanbarr@gmail.com>
2022-06-21 17:33:43 -0600

    Adjust Goofy theme to have containers be more visible.

--- packages/renderer/assets/themes/goofy.js
+++ packages/renderer/assets/themes/goofy.js
@@ -2,7 +2,7 @@ export const goofy = {
   id: "goofy",
   name: "Goofy",
   backgroundColor: "#000000",
-  containerColor: "#080808",
+  containerColor: "#212121",
   highlightColor: "#ffc933",
   textColor: "#e3e3e3",
 };
