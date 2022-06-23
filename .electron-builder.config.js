7c72d1f22b8aa2eb289034e2a8e1f823baccaf6e
Ryan Barr <ryanbarr@gmail.com>
2022-06-23 12:19:13 -0600

    Adjust how versions are determined.

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -2,7 +2,7 @@ if (process.env.VITE_APP_VERSION === undefined) {
   const now = new Date();
   process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${
     now.getUTCMonth() + 1
-  }.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
+  }.${now.getUTCDate()}${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
 }
 
 /**
