d5a84e2cd35ac4babbf3b2dab6426ad4d0508cb9
Ryan Barr <ryanbarr@gmail.com>
2022-06-24 13:48:40 -0600

    Remove a console.log from the formatValue utility.

--- packages/renderer/utils/formatValue.js
+++ packages/renderer/utils/formatValue.js
@@ -4,7 +4,5 @@ export const formatValue = (value) => {
   const chaos = (parseInt(value * 100) % 100) / 100;
   const exalts = parseInt(value - chaos);
 
-  console.log(exalts);
-
   return `~${formatPrice(exalts)}${exalts > 0 ? " " : ""}${formatPrice(chaos)}`;
 };
