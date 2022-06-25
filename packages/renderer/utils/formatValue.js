ec785401e4e8c72256045a7edaa5a3ec82d8dcde
Ryan Barr <ryanbarr@gmail.com>
2022-06-25 13:06:05 -0600

    Remove ~ from the total craft value.

--- packages/renderer/utils/formatValue.js
+++ packages/renderer/utils/formatValue.js
@@ -4,5 +4,5 @@ export const formatValue = (value) => {
   const chaos = (parseInt(value * 100) % 100) / 100;
   const exalts = parseInt(value - chaos);
 
-  return `~${formatPrice(exalts)}${exalts > 0 ? " " : ""}${formatPrice(chaos)}`;
+  return `${formatPrice(exalts)}${exalts > 0 ? " " : ""}${formatPrice(chaos)}`;
 };
