0be805cbde17efd4d32ac8940e887f63ba52c950
Ryan Barr <ryanbarr@gmail.com>
2022-06-25 13:26:36 -0600

    Fixed a bug where craft value would not appear if there was no total value.

--- packages/renderer/utils/formatValue.js
+++ packages/renderer/utils/formatValue.js
@@ -4,5 +4,9 @@ export const formatValue = (value) => {
   const chaos = (parseInt(value * 100) % 100) / 100;
   const exalts = parseInt(value - chaos);
 
+  if (chaos === 0 && exalts === 0) {
+    return "0c";
+  }
+
   return `${formatPrice(exalts)}${exalts > 0 ? " " : ""}${formatPrice(chaos)}`;
 };
