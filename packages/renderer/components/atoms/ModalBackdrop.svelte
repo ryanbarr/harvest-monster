147279a3dcfb9d37243f4001fd6a40a19d812c31
Ryan Barr <ryanbarr@gmail.com>
2022-06-27 21:53:55 -0600

    Fixed a styling issue where the modal backdrop was not rounded, causing the app to take sharp corners when a modal was present.

--- packages/renderer/components/atoms/ModalBackdrop.svelte
+++ packages/renderer/components/atoms/ModalBackdrop.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div
-  class="w-full h-full fixed bg-background opacity-75 transition-opacity z-30"
+  class="w-full h-full fixed bg-background opacity-75 transition-opacity z-30 rounded-lg"
   on:click={closeModal}
 />
